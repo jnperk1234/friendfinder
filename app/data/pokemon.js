c90d9ab11cd7b9428035f02b5bb17094aef46a2b
Jessica Perkins <jnperk1234@gmail.com>
2018-04-24 21:15:59 -0500

    upd1

---
+++ app/data/pokemon.js
@@ -0,0 +1,15 @@
+var pokemon = [{
+    "name": "Squirtle",
+    "photo" :"https://pre00.deviantart.net/044b/th/pre/f/2016/184/7/0/it_s_a_squirtle__by_nintendrawer-da8kvnx.png",
+    "scores": [4,1,1,2,5,2,1,3,5,2]
+}, {
+    "name": "Charmander",
+    "photo": "https://i.pinimg.com/originals/2f/a7/1e/2fa71efd7847bfb578be9e1e2b788efe.jpg",
+    "scores": [3,2,1,5,4,2,2,1,5,4]
+}, {
+    "name": "Bulbasaur",
+    "photo": "https://vignette.wikia.nocookie.net/pokemontowerdefensetwo/images/9/93/Bulbasaur_banner.jpg/revision/latest?cb=20160622045652",
+    "scores": [3,3,1,4,4,1,2,1,5,4]
+}];
+
+module.exports = pokemon;
